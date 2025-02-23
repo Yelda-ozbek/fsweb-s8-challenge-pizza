95ce0e699172914987ca18c2c35af8f2282b94da
yelda <yeldayazilim@icloud.com>
2025-02-23 20:40:42 +0300

    Anasayfanın yapısı eklendi.Ve css kısmı bitti.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import OrderForm from "./pages/OrderForm";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<OrderForm />} />
+      <Route path="/" element={<HomePage/>} />
+      <Route path="/order" element={<OrderForm />} />
     </Routes>
   );
 }export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./HomePage.css";
+import Logo from "./logo.svg"
+import PizzaImage from "./home-banner.png";
+
+const HomePage = ({ navigateToOrder }) => {
+  return (
+    <div className="home-container">
+      <header className="header">
+        <img src={Logo} alt="Logo"/>
+   
+      </header>
+      <main>
+        <h2>
+          <span className="homepage">KOD ACIKTIRIR<br />
+                                    PİZZA DOYURUR </span>
+        </h2>
+        <button className="bn" onClick={navigateToOrder}>ACIKTIM </button>
+        <img src={PizzaImage} alt="Pizza" className="pizza-image" />
+      </main>
+    </div>
+  );
+};
+
+export default HomePage;
